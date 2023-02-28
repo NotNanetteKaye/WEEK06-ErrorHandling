6176960bb2fc363fe8d72d83ae4474a1302a5abe
NotNanetteKaye <nanettekayedolera@gmail.com>
2023-02-27 21:37:37 -0600

    Added video tutorial comment in main.py

--- dCC_Python_SodaMachine/main.py
+++ dCC_Python_SodaMachine/main.py
@@ -3,3 +3,6 @@ from simulation import Simulation
 
 simulation = Simulation()
 simulation.run_simulation()
+
+
+# https://www.online.devcodecamp.com/courses/take/full-stack-development-flex-neptunium-main/lessons/35320639-project-walkthrough-soda-machine-debugging
